e4820c1ba0dee27f0ddd1cdaaa6028d1f63d0fd0
Your Name <you@example.com>
2022-11-24 14:39:54 -0500

    rick_morty

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import Logo from './assets/Rickmort.png'
+import { Filtrar } from './components/Filtrar'
+import { Card } from './components/Card'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [personajes, setPersonajes] = useState([])
+	const [loading, setLoading] = useState(true)
+	const [filter, setFilter] = useState('')
+
+	useEffect(() => {
+		const getPersonajes = async () => {
+			try {
+				const response = await fetch(
+					'https://rickandmortyapi.com/api/character'
+				)
+				const data = await response.json()
+				setPersonajes(data.results)
+				setLoading(false)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		getPersonajes()
+	}, [])
+
+	const personjesFiltrados = personajes.filter((personaje) =>
+		personaje.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+	)
+
+	return (
+		<div className='container'>
+			<figure className='logo'>
+				<img src={Logo} alt='Logo de Rick & Morty' />
+			</figure>
+			{/* form filtrar */}
+			<Filtrar filter={filter} setFilter={setFilter} />
+			{/* form filtrar */}
+
+			{/* section personajes */}
+			<section className='lista-personajes'>
+				{loading ? (
+					<p>Cargando...</p>
+				) : personjesFiltrados.length > 0 ? (
+					personjesFiltrados.map((personaje) => (
+						<Card key={personaje.id} personaje={personaje} />
+					))
+				) : (
+					<p>
+						No se encontro personajes con la busqueda{' '}
+						<strong>"{filter}"</strong>.
+					</p>
+				)}
+			</section>
+			{/* section personajes */}
+		</div>
+	)
 }
 
-export default App;
+export default App
